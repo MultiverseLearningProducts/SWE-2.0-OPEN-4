7260a5863f7fbe756879b933fafe2f47da978b73
= <90lineng@gmail.com>
2023-01-25 17:08:46 -0500

    week 1 updated

---
+++ 01-Week1-DO-NOT-EDIT/2-variables-constants-expressions/lesson.js
@@ -0,0 +1,29 @@
+//let variable
+let age = 25
+age = 30
+age = false
+
+//const variable
+const port = '3000'
+
+
+// Expressions
+let combineString = "hello " + "world" + "!"
+
+console.log(combineString)
+
+
+// hello this is a comment
+
+/* 
+
+
+
+
+write your comment 
+
+
+
+
+
+*/
