5888e0f28c72143d7ca77c6b555fbb6f2001560e
= <90lineng@gmail.com>
2023-01-25 11:00:02 -0500

    Day 3 templates uploaded

---
+++ 01-Week1-DO-NOT-EDIT/3-data-primitives-conditionals/lesson.js
@@ -0,0 +1,18 @@
+//COPY ME TO YOUR PERSONAL FOLDER - DO NOT EDIT HERE
+//Warm up exercise
+//Determine whether or not the following is appropriately labeled? 
+let a = '4'
+const height = 35
+
+//Control Flow
+
+
+//Comparison Operators
+
+
+
+//Logical Operators
+
+
+
+//Pair Programming exercise
