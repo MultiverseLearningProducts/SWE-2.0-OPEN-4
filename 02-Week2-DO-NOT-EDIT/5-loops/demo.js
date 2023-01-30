0ae456e821fecc402007a8a5c55d5a54bb1a9155
= <90lineng@gmail.com>
2023-01-30 10:31:09 -0500

    week-2 day 1: loops added

---
+++ 02-Week2-DO-NOT-EDIT/5-loops/demo.js
@@ -0,0 +1,26 @@
+/* 
+Warm up exercise:
+
+Pair-program - Using what we know so far, create a function that will count to 10.  
+
+Extension - create a function that will sing happy birthday to you.
+
+Lyrics:
+Happy Birthday to you
+Happy Birthday to you
+Happy Birthday dear ${yourName}
+Happy Birthday to you
+
+*/
+
+
+//For Loop Demo
+
+    /* Syntax */
+    
+    /* Counter */
+
+    /* Iterate through string */
+
+//While Loop Demo
+
