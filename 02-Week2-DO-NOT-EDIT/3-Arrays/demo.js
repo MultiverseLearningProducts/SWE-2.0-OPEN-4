ba57ca1d9399b8c770e64b8cc59d4d12df870d0a
= <90lineng@gmail.com>
2023-02-01 10:39:23 -0500

    array notes added

---
+++ 02-Week2-DO-NOT-EDIT/3-Arrays/demo.js
@@ -0,0 +1,19 @@
+/* warm up */
+
+/*
+    This string is composed of team players. Determine if the game is balanced. A game is balanced if there are equal amount of players.
+    If the game is balanced -> return the string 'game is balanced!'
+    If the game is unbalanced -> return a string 'game is unbalanced! Team ${team that has more} is more likely to win!'
+
+    Input: 'xxooxo' Output: 'game is balanced!'
+    Input: 'xxxxoxo' Output: 'game is unbalanced! Team x is more likely to win!'
+    Input: 'xXxoOo' Output: 'game is balanced!'
+
+    Hint: 
+    1. Use a loop. To access a character in a string: 
+    //            0123
+    const name = 'abcd' // access by index
+    name[2] // 'c'
+    2. you might want to use .toUpperCase() or .toLowerCase()
+*/
+
