81b897feb01bf9fcb922b106cf78537f3db46265
= <90lineng@gmail.com>
2023-02-06 10:56:27 -0500

    recursion warm up added

---
+++ 03-Week3-DO-NOT-EDIT/1-Git/warmup.js
@@ -0,0 +1,5 @@
+/* 
+1. Write a function called countDown that uses recursion to count down from 10 to 1.
+2. Write a function called empty this array that uses recursion to remove all elements of an array
+*/
+
