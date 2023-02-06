c5a62948a53400bbec1acb7d9874f53ebf57e5d4
= <90lineng@gmail.com>
2023-02-06 12:19:17 -0500

    added git notes

--- 03-Week3-DO-NOT-EDIT/1-Git/warmup.js
+++ 03-Week3-DO-NOT-EDIT/1-Git/warmup.js
@@ -3,3 +3,31 @@
 2. Write a function called empty this array that uses recursion to remove all elements of an array
 */
 
+function countDown(number) {
+    //base Case - aka the stopping point - 
+    if(number === 1) {
+        console.log(1)
+        return 
+    }   
+    //recursive Case
+    console.log(number)
+    countDown(number - 1)
+}
+
+countDown(10)
+
+// 
+//call stack
+
+
+
+function emptyThisArray(arr) {
+    //base Case - when the array has no elements aka []
+    if(arr.length < 1) {
+        return
+    }
+
+    //recursive Case - if there are still elements in the array
+    arr.pop()
+    emptyThisArray(arr)
+}
