0583e21854b125dd163c3b6bece77afd8e302f50
= <90lineng@gmail.com>
2023-03-15 12:38:52 -0400

    react state exercises added

--- part-2/02-week-5/react-state/react-state-demo/client/components/Counter.js
+++ part-2/02-week-5/react-state/react-state-demo/client/components/Counter.js
@@ -1,4 +1,5 @@
-import {React, useState} from 'react' //import useState hook
+import React from 'react' //import useState hook
+import {useState} from 'react'
 
 
 function Counter() {
@@ -12,10 +13,14 @@ function Counter() {
     setCount(count + 1)
   }
 
+  //exercise 1: Create a decrement function and button to update state
+  //exercise 2: After 10 clicks, render new text within the paragraph tag: "Thats a lot of clicks!" hint: ternary 
+  //exercise 3: Create a button that will reset the counter
+
   return (
       <div>
        <p>You clicked the button {count} times!</p>
-        <button onClick={()=>updateCount}>Click Me!!</button>
+        <button onClick={updateCount}>Click Me!!</button>
       </div>
   )
 }
--- part-2/02-week-5/react-state/react-state-demo/dist/client.e31bb0bc.js
+++ part-2/02-week-5/react-state/react-state-demo/dist/client.e31bb0bc.js
@@ -28891,15 +28891,15 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.default = void 0;
-var _react = require("react");
+var _react = _interopRequireWildcard(require("react"));
+function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "function") return null; var cacheBabelInterop = new WeakMap(); var cacheNodeInterop = new WeakMap(); return (_getRequireWildcardCache = function (nodeInterop) { return nodeInterop ? cacheNodeInterop : cacheBabelInterop; })(nodeInterop); }
+function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || typeof obj !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
 function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
 function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
 function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) arr2[i] = arr[i]; return arr2; }
 function _iterableToArrayLimit(arr, i) { var _i = arr == null ? null : typeof Symbol !== "undefined" && arr[Symbol.iterator] || arr["@@iterator"]; if (_i == null) return; var _arr = []; var _n = true; var _d = false; var _s, _e; try { for (_i = _i.call(arr); !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
 function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
-//import useState hook
-
 function Counter() {
   //declare a state variable - set an initial value of your state
   //state   function that updates state
@@ -28913,10 +28913,13 @@ function Counter() {
     //use setCount to update our state
     setCount(count + 1);
   }
-  return /*#__PURE__*/_react.React.createElement("div", null, /*#__PURE__*/_react.React.createElement("p", null, "You clicked the button ", count, " times!"), /*#__PURE__*/_react.React.createElement("button", {
-    onClick: function onClick() {
-      return updateCount;
-    }
+
+  //exercise 1: Create a decrement function and button to update state
+  //exercise 2: After 10 clicks, render new text within the paragraph tag: "Thats a lot of clicks!" hint: ternary 
+  //exercise 3: Create a button that will reset the counter
+
+  return /*#__PURE__*/_react.default.createElement("div", null, /*#__PURE__*/_react.default.createElement("p", null, "You clicked the button ", count, " times!"), /*#__PURE__*/_react.default.createElement("button", {
+    onClick: updateCount
   }, "Click Me!!"));
 }
 var _default = Counter;
@@ -29049,7 +29052,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64334" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64689" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
